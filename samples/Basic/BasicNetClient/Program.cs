59f9c01d5362f814b057c4578a97099fa2d9e05a
ldicocco <lu.dicocco@gmail.com>
2013-11-25 18:30:27 +0100

    Sliced file result supported

---
+++ samples/Basic/BasicNetClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BridgedRpc.Client;
+
+namespace BasicNetClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var proxy = new RpcServerProxy("server01", "http://localhost:58355");
+
+			Console.WriteLine("Ready");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ src/BridgedRpc.Client/RpcServerProxy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using Microsoft.AspNet.SignalR.Client;
+
+namespace BridgedRpc.Client
+{
+	public class RpcServerProxy
+	{
+		public string ServerName { get; private set; }
+		public Uri BaseUri { get; private set; }
+		public Connection Connection { get; private set; }
+		private HttpClient HttpClient { get; set; }
+		private string _connectionPath;
+		private string _rpcPath;
+
+		public RpcServerProxy(string serverName, string baseUri, string connectionPath = "/BridgedRpc", string rpcPath = "/rpc/RpcBridge")
+		{
+			BaseUri = new Uri(baseUri);
+			_connectionPath = connectionPath;
+			_rpcPath = rpcPath;
+			Connection = new Connection(BaseUri.Append(connectionPath).ToString());
+			ServerName = serverName;
+		}
+
+	}
+}
